67bfb9f082e406efedcc6c298e7d48be69c53edd
Fillipe <fillipe.fbr@pf.gov.br>
2024-03-18 13:00:01 -0300

    Adição de novos listeners

--- main.js
+++ main.js
@@ -20,6 +20,19 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
         })
     });
 
+    client.on('loading_screen', (percent, message) => {
+        console.log('LOADING SCREEN', percent, message);
+    });
+
+    client.on('authenticated', () => {
+        console.log('AUTHENTICATED');
+    });
+
+    client.on('auth_failure', msg => {
+        // Fired if session restore was unsuccessful
+        console.error('AUTHENTICATION FAILURE', msg);
+    });
+
     client.on('ready', () => {
         console.log('Client is ready!');
     });
@@ -31,14 +44,31 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
     client.on('remote_session_saved', () => {
         console.log("session saved");
     });
+
+    client.on('disconnected', (reason) => {
+        console.log('Client was logged out', reason);
+    });
+
+    client.on('message', async msg => {
+        console.log('MESSAGE RECEIVED', msg);
+
+        if (msg.body === '!chats') {
+            const chats = await client.getChats();
+            client.sendMessage(msg.from, `The bot has ${chats.length} chats open.`);
+        }
+    });
     
     // Listening to all incoming messages
-    client.on('message_create', message => {
+    client.on('message_create', async message => {
         console.log(message.body);
     
         if (message.body === '!ping') {
             // send back "pong" to the chat the message was sent in
             client.sendMessage(message.from, 'pong');
+        }
+        else if (message.body === '!chats') {
+            const chats = await client.getChats();
+            client.sendMessage(message.from, `The bot has ${chats.length} chats open.`);
         }
     });
 
