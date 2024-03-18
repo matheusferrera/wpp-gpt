58a4db254862c85b87d1b94cf56ded5f3e29c394
Fillipe <fillipe.fbr@pf.gov.br>
2024-03-18 12:22:01 -0300

    Configuração de remoteAuth

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
-const { Client, NoAuth } = require('whatsapp-web.js');
+const { Client, RemoteAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
+// Require database
+const { MongoStore } = require('wwebjs-mongo');
+const mongoose = require('mongoose');
 
-const client = new Client({
-    authStrategy: new NoAuth()
+// Setup environment variables
+require('dotenv').config();
+
+// Load the session data
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+
+    const store = new MongoStore({ mongoose: mongoose });
+    const client = new Client({
+        authStrategy: new RemoteAuth({
+            clientId: 'client_01',
+            store: store,
+            backupSyncIntervalMs: 300000
+        })
+    });
+
+    client.on('ready', () => {
+        console.log('Client is ready!');
+    });
+    
+    client.on('qr', qr => {
+        qrcode.generate(qr, {small: true});
+    });
+
+    client.on('remote_session_saved', () => {
+        console.log("session saved");
+    });
+    
+    // Listening to all incoming messages
+    client.on('message_create', message => {
+        console.log(message.body);
+    
+        if (message.body === '!ping') {
+            // send back "pong" to the chat the message was sent in
+            client.sendMessage(message.from, 'pong');
+        }
+    });
+
+    client.initialize();
 });
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-});
-
-// Listening to all incoming messages
-client.on('message_create', message => {
-	console.log(message.body);
-
-    if (message.body === '!ping') {
-		// send back "pong" to the chat the message was sent in
-		client.sendMessage(message.from, 'pong');
-	}
-});
-
-
-client.initialize();
