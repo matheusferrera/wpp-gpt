96a960a4c79a768bc96497e79a7c68b82c96d501
Fillipe <fillipefbr@hotmail.com>
2024-03-26 17:44:56 -0300

    conversão do código fonte para TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,92 @@
-// src/index.js
 import express, { Express, Request, Response } from "express";
+import http from "http";
+import { Server } from "socket.io";
+import { Client, RemoteAuth } from "whatsapp-web.js";
+import qrcode from "qrcode-terminal";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'client_01';
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+const app: Express = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+      origin: '*',
+    }
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+// Require database
+import { MongoStore } from "wwebjs-mongo";
+import mongoose from "mongoose";
+
+
+// WhatsApp clients map to store client instances by clientId
+const whatsappClients = new Map();
+
+// Function to initialize WhatsApp client with clientId
+function initializeWhatsAppClient(clientId: any) {
+
+    mongoose.connect(MONGODB_URI).then(() => {
+        const store = new MongoStore({ mongoose: mongoose });
+        const whatsappClient = new Client({
+            authStrategy: new RemoteAuth({
+                clientId: clientId,
+                store: store,
+                backupSyncIntervalMs: 300000
+            })
+        });
+        
+        whatsappClient.on('qr', (qr) => {
+            qrcode.generate(qr, { small: true });
+        });
+        
+        whatsappClient.on('ready', () => {
+            console.log(`WhatsApp client (${clientId}) is ready`);
+        });
+        
+        whatsappClient.on('message', (message) => {
+            // Broadcast incoming messages to the corresponding WebSocket client
+            io.to(clientId).emit('message-received', message);
+        });
+        
+        whatsappClient.initialize();
+        
+        // Store the client instance in the map
+        whatsappClients.set(clientId, whatsappClient);
+    });
+
+}
+
+// WebSocket connection event
+io.on('connection', (socket) => {
+    console.log('A client connected');
+
+    // Receive client ID from the client
+    const clientId = socket.handshake.query.clientId;
+
+    // Check existence of received clientId query
+    if(clientId && typeof clientId !== "undefined") {
+        // Join the room corresponding to the client ID
+        socket.join(clientId);
+
+        // Initialize WhatsApp client if not already initialized
+        if (!whatsappClients.has(clientId)) {
+            initializeWhatsAppClient(clientId);
+        }
+    }
+
+    // Handle disconnection
+    socket.on('disconnect', () => {
+        console.log('A client disconnected');
+    });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
