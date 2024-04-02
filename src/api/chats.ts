995a58b4526bfe9e7674c2c558e7f8287f3c1f57
Fillipe <fillipefbr@hotmail.com>
2024-04-02 16:26:48 -0300

    atualização da API

--- src/api/chats.ts
+++ src/api/chats.ts
@@ -1,10 +1,24 @@
 import express, { Router, Request, Response } from "express";
+import UserModel from "../models/User";
 
 const router: Router = express.Router();
-// POST /user/signin
-router.get('/test', async (req: Request, res: Response) => {
+
+// GET /chats/test
+router.get('/', async (req: Request, res: Response) => {
     try {
         res.send('OK');
+    } catch (e: any) {
+        res.status(500).send(e.toString());
+    }
+});
+
+// GET /chats/{clientId}/{userId}
+router.get('/:clientId/:userId', async (req: Request, res: Response) => {
+    try {
+        const clientId = req.params.clientId;
+        const userId = req.params.userId;
+        const user = await UserModel.findOne({ clientId: clientId, userId: userId });
+        res.send(user);
     } catch (e: any) {
         res.status(500).send(e.toString());
     }
