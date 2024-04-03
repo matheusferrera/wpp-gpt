99d181e1160ae9211d0ee85ccae366114d3230f1
Fillipe <fillipefbr@hotmail.com>
2024-04-03 17:38:17 -0300

    atualização da documentação do Swagger para /clients

--- src/routes/clients.ts
+++ src/routes/clients.ts
@@ -23,6 +23,107 @@
  *         name: Loja 1
  */
 
+/**
+ * @swagger
+ * tags:
+ *   name: Clients
+ *   description: The clients
+ * /clients:
+ *   get:
+ *     summary: Lists all the clients
+ *     tags: [Clients]
+ *     responses:
+ *       200:
+ *         description: The list of the saved clients
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Client'
+ *   post:
+ *     summary: Create a new client
+ *     tags: [Clients]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Client'
+ *     responses:
+ *       200:
+ *         description: The created client.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Client'
+ *       500:
+ *         description: Some server error
+ * /clients/{clientId}:
+ *   get:
+ *     summary: Get the client by id
+ *     tags: [Clients]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The client id
+ *     responses:
+ *       200:
+ *         description: The client response by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Client'
+ *       404:
+ *         description: The book was not found
+ *   put:
+ *    summary: Update the client by the id
+ *    tags: [Clients]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The client id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Client'
+ *    responses:
+ *      200:
+ *        description: The book was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Client'
+ *      404:
+ *        description: The client was not found
+ *      500:
+ *        description: Some error happened
+ *   delete:
+ *     summary: Remove the client by id
+ *     tags: [Clients]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The client id
+ *
+ *     responses:
+ *       200:
+ *         description: The client was deleted
+ *       404:
+ *         description: The client was not found
+ */
+
 import express, { Router, Request, Response } from "express";
 import ClientModel from "../models/Client";
 
