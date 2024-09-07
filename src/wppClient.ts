b135283e766bf5bb8b892a81ab442f914774b03d
root <root@ip-172-31-22-93.eu-north-1.compute.internal>
2024-09-07 16:44:41 +0000

    atualizando cors

--- src/wppClient.ts
+++ src/wppClient.ts
@@ -13,7 +13,7 @@ async function initializeWhatsAppClient(): Promise<Client> {
       dataPath: "client_wpp",
     }),
     puppeteer: {
-      // executablePath: "/usr/bin/chromium-browser",
+      executablePath: "/usr/bin/chromium-browser",
       //headless: false, // Inicia o navegador e abre o wpp
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     },
